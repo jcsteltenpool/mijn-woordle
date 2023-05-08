21158ca44cb45549e47cc70ab0c713ed985095c1
Joost Steltenpool <jcsteltenpool@users.noreply.github.com>
2023-05-08 09:51:23 +0200

    create handleKeyDown and handleGuess v1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,73 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import GameContainer from "./components/GameContainer";
-import GridContainer from "./components/GridContainer";
+import Grid from "./components/GridContainer";
 import KeyboardContainer from "./components/KeyboardContainer";
+import { KEYBOARD_KEYS } from "./util/keyboard_keys";
+
 
 
 export default function App() {
   const [isEvaluated, setIsEvaluated] = useState(false);
+  const [guess, setGuess] = useState(Array(5).fill(null));
+  const [tile, setTile] = useState(0);
+
+  useEffect(() => {
+    function handleKeyDown(e) {
+      KEYBOARD_KEYS.forEach(key => {
+        if (e.key === key) {
+          handleClick(e.key);
+        }
+      })
+    }
+    document.addEventListener('keydown', handleKeyDown);
+    
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    }
+  });
+  
+  function handleClick(keyValue) {
+    if (keyValue === 'Enter' ) {
+      return
+    } else if (keyValue === 'Backspace') {
+      removeLastLetter();
+    } else {
+      handleNextGuess(keyValue);
+    }
+  }
+  
+  function handleNextGuess(keyValue) {
+    if (tile === 5) {
+      return
+    } else {
+      const nextGuess = guess.slice();
+      nextGuess[tile] = keyValue;
+      setGuess(nextGuess);
+      const nextTile = tile + 1;
+      setTile(nextTile);
+    }
+  }
+
+  function removeLastLetter() {
+    if (tile === 0) {
+      return
+    } else {
+      const prevGuess = guess.slice();
+      prevGuess[tile - 1] = null;
+      setGuess(prevGuess);
+      const prevTile = tile - 1;
+      setTile(prevTile);
+    }
+  }
 
   return (
     <>
       <button onClick={() => setIsEvaluated(!isEvaluated)}>Test</button>
-      <GridContainer isEvaluated={isEvaluated} />
-      <KeyboardContainer />
+      <Grid isEvaluated={isEvaluated}
+            guess={guess} />
+      <p>Value: {guess.toString()}</p>
+      <p>Target tile: {tile}</p>
+      <KeyboardContainer onKeyboardClick={handleClick}/>
     </>
   );
 }
--- src/components/GridContainer.js
+++ src/components/GridContainer.js
@@ -1,37 +1,65 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-function Tile({ isEvaluated }) {
+function Tile({ value, isEvaluated }) {
+    const [status, setStatus] = useState("empty")
+
+    useEffect(() => {
+        if (value === null){
+            setStatus("empty");
+        } else {
+            setStatus("tbd")
+        }
+    }, [value]);
+
     return (
         <div className="game-tile">
-            <div data-state="empty" data-visible={!isEvaluated}>m</div>
-            <div data-state="correct" data-visible={isEvaluated}>k</div>
+            <div data-status={status} data-visible={!isEvaluated}>{value}</div>
+            <div data-status="correct" data-visible={isEvaluated}>{value}</div>
         </div>
     )
 }
 
-function Row({ isEvaluated }) {
+function Row({ guess, isEvaluated }) {
     return (
         <div className="game-grid-row">
-            {Array.from({ length: 5 }, (tile, index) => (
-                <Tile key={index} isEvaluated={isEvaluated} />
+            {Array.from({ length: 5}, (tile, index) => (
+                <Tile key={index}
+                      value={guess[index]}
+                      isEvaluated={isEvaluated} />  
             ))}
         </div>
     )
 }
 
-function Grid({ isEvaluated }) {
+export default function Grid({ guess, isEvaluated }) {
     return (
         <div className="game-grid-wrapper">
             <div className="game-grid">
                 {Array.from({ length: 6}, (row, index) => (
-                    <Row key={index} isEvaluated={isEvaluated} />
+                    <Row key={index}
+                         guess={guess}
+                         isEvaluated={isEvaluated} />
                 ))}
             </div>
         </div>
     )
 }
 
-
-export default function GridContainer({ isEvaluated }) {
-    return <Grid isEvaluated={isEvaluated}/>
-}
+// export default function GridContainer({ keyValue, isEvaluated }) {
+//     return (
+//         <div className="game-grid-wrapper">
+//             <div className="game-grid">
+//                 {Array.from({ length: 6}, (row, index) => (
+//                     <div key={index} className="game-grid-row">
+//                         {Array.from({ length: 5 }, (tile, index) => (
+//                             <div key={index} className="game-tile">
+//                                 <div data-state="empty" data-visible={!isEvaluated}>{keyValue}</div>
+//                                 <div data-state="correct" data-visible={isEvaluated}>{keyValue}</div>
+//                             </div>
+//                         ))}
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     )
+// }
