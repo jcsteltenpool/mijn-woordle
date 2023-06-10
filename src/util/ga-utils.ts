fff4acf788f72fb0e0e96998842aa187793e087a
Joost Steltenpool <jcsteltenpool@users.noreply.github.com>
2023-06-10 10:49:19 +0200

    update initGA

--- src/util/ga-utils.ts
+++ src/util/ga-utils.ts
@@ -1,9 +1,9 @@
 import ReactGA from "react-ga4";
 
 export const initGA = (id: string) => {
-    ReactGA.initialize(id);
+    // ReactGA.initialize(id);
 
-    // if (process.env.NODE_ENV === "production") {
-    //     ReactGA.initialize(id);
-    // }
+    if (process.env.NODE_ENV === "production") {
+        ReactGA.initialize(id);
+    }
 }
