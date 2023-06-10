4d1b90808aa180f6bbe50bb0c88aef01d7e69fc2
Joost Steltenpool <jcsteltenpool@users.noreply.github.com>
2023-06-10 10:54:27 +0200

    initGA test1

--- src/util/ga-utils.ts
+++ src/util/ga-utils.ts
@@ -1,9 +1,9 @@
 import ReactGA from "react-ga4";
 
 export const initGA = (id: string) => {
-    // ReactGA.initialize(id);
+    ReactGA.initialize("G-12MS71069J");
 
-    if (process.env.NODE_ENV === "production") {
-        ReactGA.initialize(id);
-    }
+    // if (process.env.NODE_ENV === "production") {
+    //     ReactGA.initialize(id);
+    // }
 }
